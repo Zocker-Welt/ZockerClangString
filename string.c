ec02ffbc3e1086beddf5ab643c91814d1d056cb5
Zocker Welt <166701868+Zocker-Welt@users.noreply.github.com>
2025-03-07 15:45:43 +0300

    fixed/reworked string.c

--- string.c
+++ string.c
@@ -2,108 +2,70 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_STRING_LENGTH 2048
 #define at *String_at
 
-typedef struct String {
-    char data[MAX_STRING_LENGTH];
-    int length;
+typedef struct {
+    char* data;
+    size_t length;
 } String;
 
-void StringLengthError() {
-    fprintf(stderr, "String limit (%d) exceeded\n", MAX_STRING_LENGTH);
-    exit(1);
+String str(char* s) {
+    String new_str;
+    new_str.length = strlen(s);
+    new_str.data = malloc(new_str.length + 1);
+    strcpy(new_str.data, s);
+    return new_str;
 }
 
-void IndexError() {
-    fprintf(stderr, "Index out of range\n");
-    exit(1);
+String empty() {
+    String new_str;
+    new_str.length = 0;
+    new_str.data = malloc(1);
+    new_str.data[0] = '\0';
+    return new_str;
 }
 
-void String_init(String* self, char* data) {
-    int length = strlen(data);
-    if (length == 0) {
-        length = 1;
+void sprint(String s) {
+    printf("%s\n", s.data);
+}
+
+void free_string(String* s) {
+    free(s->data);
+    s->data = NULL;
+    s->length = 0;
+}
+
+String concat(String s1, String s2) {
+    String new_str;
+    new_str.length = s1.length + s2.length;
+    new_str.data = malloc(new_str.length + 1);
+    strcpy(new_str.data, s1.data);
+    strcat(new_str.data, s2.data);
+    return new_str;
+}
+
+String slice(String s, size_t start, size_t end) {
+    if (start > s.length || end > s.length || start > end) {
+        String new_str;
+        new_str.length = 0;
+        new_str.data = malloc(1);
+        new_str.data[0] = '\0';
+        return new_str;
     }
-    self->length = 0;
-    self->data[0] = '\0';
-    if (!(length > 0 && length < MAX_STRING_LENGTH)) StringLengthError;
-    strncpy(self->data, data, length);
-    self->data[length] = '\0';
-    self->length = length;
+
+    String new_str;
+    new_str.length = end - start;
+    new_str.data = malloc(new_str.length + 1);
+    strncpy(new_str.data, s.data + start, new_str.length);
+    new_str.data[new_str.length] = '\0';
+    return new_str;
 }
 
-String* string(char* data) {
-    String* self = malloc(sizeof(String));
-    String_init(self, data);
-    return self;
+char* String_at(String s, int index) {
+    if (index < 0 || (size_t)index >= s.length) return NULL;
+    return s.data + (size_t)index;
 }
 
-String* empty() {
-    String* self = malloc(sizeof(String));
-    String_init(self, "");
-    return self;
-}
-
-String* plus(String* data1, String* data2) {
-    String* result = data1;
-    if (!(data1->length + data2->length < MAX_STRING_LENGTH)) StringLengthError();
-    strncat(result->data, data2->data, data2->length);
-    result->length += data2->length;
-}
-
-String* mul(String* data1, int n) {
-    String* result = empty();
-    for (int i = 0; i < n; i++) {
-        result = plus(result, data1);
-    }
-    return result;
-}
-
-/*
-char at(String* self, int idx) {
-    if (idx < 0 || idx >= self->length) IndexError();
-    return self->data[idx];
-}
-*/
-
-char* String_at(String* self, int idx) {
-    if (idx < 0 || idx >= self->length) IndexError();
-    return &self->data[idx];
-}
-
-char* char_end(char chr) {
-    char* temp = malloc(2 * sizeof(char));
-    temp[0] = chr;
-    temp[1] = '\0';
-    return temp;
-}
-
-char* charptr_end(char* _str) {
-    char* temp = malloc((strlen(_str) + 1) * sizeof(char));
-    strcpy(temp, _str);
-    return temp;
-}
-
-String* slice(String* self, int idx1, int idx2) {
-    if (idx1 < 0 || idx1 > self->length) IndexError();
-    if (idx2 < 0 || idx2 > self->length) IndexError();
-    if (idx1 > idx2) IndexError();
-    String* result = empty();
-    for (int i = idx1; i < idx2; i++) {
-        result = plus(result, string(char_end(self->data[i])));
-    }
-    return result;
-}
-
-String* pop(String* self, int idx) {
-    if (idx < 0 || idx >= self->length) IndexError();
-    String* result = empty();
-    result = plus(result, slice(self, 0, idx));
-    result = plus(result, slice(self, idx + 1, self->length));
-    return result;
-}
-
-void sprint(String* data) {
-    printf("%s", data->data);
+int compare(String s1, String s2) {
+    return strcmp(s1.data, s2.data);
 }
